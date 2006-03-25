3bbdc27df7bb022c86c29221535df535a6a3cd56
Cliff Ingham <inghamn@gmail.com>
2006-03-25 16:55:59 +0000

    Got categories working in the new object oriented way

--- html/home.php
+++ html/home.php
@@ -4,17 +4,22 @@
 	include(APPLICATION_HOME."/includes/controls.inc");
 	include(APPLICATION_HOME."/includes/categories.inc");
 
-	if (!isset($_SESSION['USER_ID']) || !$_SESSION['USER_ID']) { include(APPLICATION_HOME."/includes/toolboxes/loginForm.inc"); }
-	if(isset($_SESSION['ROLES']) && in_array("Administrator",$_SESSION['ROLES'])) { include (APPLICATION_HOME."/includes/toolboxes/admin.inc"); }
+	if (!isset($_SESSION['USER'])) { include(APPLICATION_HOME."/includes/toolboxes/loginForm.inc"); }
+	if(isset($_SESSION['USER']) && in_array("Administrator",$_SESSION['USER']->getRoles())) { include (APPLICATION_HOME."/includes/toolboxes/admin.inc"); }
 ?>
 
 <div id="mainContent">
 	<?php
+		require_once(APPLICATION_HOME."/classes/PhotoList.inc");
 		$photoList = new PhotoList();
 		$photoList->find();
 		foreach($photoList as $photo)
 		{
-			echo "<div>$photo->getName();</div>";
+			echo "
+			<div><img src=\"smalls/{$photo->getFilename()}\" alt=\"{$photo->getFilename()}\" />
+				<p>{$photo->getDate()}</p>
+			</div>
+			";
 		}
 	?>
 </div>
